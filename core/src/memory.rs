bcaf3ef811d42239687c5e702719d8a6c14d7875
Lukas Krickl <lukas@krickl.dev>
2021-05-13 07:55:11 +0200

    Added ref cell

--- core/src/memory.rs
+++ core/src/memory.rs
@@ -8,4 +8,42 @@ pub unsafe extern "C" fn umemset(ptr: *mut u8, value: u8, size: usize) -> *mut u
     return ptr;
 }
 
+/**
+ * Simple shared reference implementation
+ * no bells and whistles, just a way to pass
+ * values with shared ownership
+ * since there is no allocator yet we use an
+ * unsafe pointer for now!
+ * Once we can use something similar the Box use it!
+ */
+#[derive(Copy, Clone)]
+pub struct SharedPtrCell<T> {
+    ptr: *mut T
+}
+
+impl<'a, T> SharedPtrCell<T> {
+    pub fn new(ptr: *mut T) -> Self {
+        Self {
+            ptr
+        }
+    }
+
+    /**
+     * Of course this is so unsafe it's not even funny
+     * but we should not usually panic!
+     */
+    pub fn as_ref(&self) -> &'a T {
+        unsafe {
+            return self.ptr.as_ref().unwrap();
+        }
+    }
+
+    pub fn as_mut(&mut self) -> &'a T {
+        unsafe {
+            return self.ptr.as_mut().unwrap();
+        }
+    }
+}
+
+
 
